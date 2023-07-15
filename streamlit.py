fcb582b379fe4af6bc1505f291f5071f28f828e1
Tanirika Roy <81459933+TanirikaRoy@users.noreply.github.com>
2023-07-15 14:48:40 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,34 +1,56 @@
 import streamlit as st
 
 def main():
-    st.title('My College Application')
-    
-    menu = ['About Me', 'Academics', 'Activities', 'Contact']
-    choice = st.sidebar.selectbox('Menu', menu)
-    
-    if choice == 'About Me':
-        about_me()
-    elif choice == 'Academics':
-        academics()
-    elif choice == 'Activities':
-        activities()
-    elif choice == 'Contact':
-        contact()
-    
-def about_me():
-    st.header('About Me')
+    st.set_page_config(page_title='College Application', layout='wide')
+
+    # Define color theme
+    primary_color = '#8b0000'  # Dark red
+    secondary_color = '#ffd700'  # Gold
+
+    # Header
+    st.markdown(
+        f'<h1 style="color: {primary_color}; text-align: center;">My College Application</h1>',
+        unsafe_allow_html=True
+    )
+    st.write('')
+
+    # Navigation buttons
+    st.markdown(
+        f'<div style="text-align: center;">'
+        f'<a href="#about" style="background-color: {secondary_color}; '
+        f'color: {primary_color}; padding: 10px 20px; margin-right: 10px; border-radius: 5px; '
+        f'text-decoration: none; font-weight: bold;">About Me</a>'
+        f'<a href="#academics" style="background-color: {secondary_color}; '
+        f'color: {primary_color}; padding: 10px 20px; margin-right: 10px; border-radius: 5px; '
+        f'text-decoration: none; font-weight: bold;">Academics</a>'
+        f'<a href="#activities" style="background-color: {secondary_color}; '
+        f'color: {primary_color}; padding: 10px 20px; margin-right: 10px; border-radius: 5px; '
+        f'text-decoration: none; font-weight: bold;">Activities</a>'
+        f'<a href="#contact" style="background-color: {secondary_color}; '
+        f'color: {primary_color}; padding: 10px 20px; margin-right: 10px; border-radius: 5px; '
+        f'text-decoration: none; font-weight: bold;">Contact</a>'
+        f'</div>',
+        unsafe_allow_html=True
+    )
+    st.write('')
+
+    # About Me section
+    st.markdown('<h2 id="about" style="color: black;">About Me</h2>', unsafe_allow_html=True)
     st.write('Write a brief introduction about yourself, your background, and your interests.')
+    st.write('')
 
-def academics():
-    st.header('Academics')
+    # Academics section
+    st.markdown('<h2 id="academics" style="color: black;">Academics</h2>', unsafe_allow_html=True)
     st.write('Highlight your academic achievements, courses, GPA, and any honors or awards.')
+    st.write('')
 
-def activities():
-    st.header('Activities')
+    # Activities section
+    st.markdown('<h2 id="activities" style="color: black;">Activities</h2>', unsafe_allow_html=True)
     st.write('Showcase your extracurricular activities, leadership roles, and community involvement.')
+    st.write('')
 
-def contact():
-    st.header('Contact')
+    # Contact section
+    st.markdown('<h2 id="contact" style="color: black;">Contact</h2>', unsafe_allow_html=True)
     st.write('Provide your contact information for college admissions officers to reach you.')
     st.write('- Email: yourname@example.com')
     st.write('- Phone: (123) 456-7890')
